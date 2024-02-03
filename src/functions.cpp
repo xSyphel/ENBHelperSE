73ba1cc5ab4514181c5cf72060060fb9fe24986d
Syphel <53709860+xSyphel@users.noreply.github.com>
2024-02-03 23:24:02 +0100

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -79,26 +79,6 @@ extern "C" DLLEXPORT bool GetWorldSpaceID(std::uint32_t& worldSpaceFormID)
 	return false;
 }
 
-// exp
-extern "C" DLLEXPORT bool GetFogData(RE::TESWeather* fogData)
-{
-	const auto weatherPtr = RE::TESWeather::FogData();
-	    
-    if (weatherPtr && weatherPtr->currentLocation) {
-        fogData.dayNear = weatherPtr->dayNear;
-        fogData.dayFar = weatherPtr->dayFar;
-        fogData.nightNear = weatherPtr->nightNear;
-        fogData.nightFar = weatherPtr->nightFar;
-        fogData.dayPower = weatherPtr->dayPower;
-        fogData.nightPower = weatherPtr->nightPower;
-        fogData.dayMax = weatherPtr->dayMax;
-        fogData.nightMax = weatherPtr->nightMax;
-
-        return true;
-    }
-    return false;
-}
-
 // Papyrus Weather.GetClassification
 extern "C" DLLEXPORT int32_t GetClassification(RE::TESWeather* weather)
 {
