82134e02b7aa787f1eef7b4f1b5ac77043bec404
Syphel <53709860+xSyphel@users.noreply.github.com>
2024-02-03 23:03:31 +0100

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -80,7 +80,7 @@ extern "C" DLLEXPORT bool GetWorldSpaceID(std::uint32_t& worldSpaceFormID)
 }
 
 // exp
-extern "C" DLLEXPORT bool GetFogData(FogData& fogData)
+extern "C" DLLEXPORT bool GetFogData(RE::TESWeather* fogData)
 {
 	const auto weatherPtr = RE::TESWeather::GetSingleton();
 	    
